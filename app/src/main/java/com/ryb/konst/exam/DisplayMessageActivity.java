6186f9a584619ff418a73379570d364ede2360ae
krybmaster <rybakk1@gmail.com>
2018-12-26 14:10:05 +0300

    получаем данные

--- app/src/main/java/com/ryb/konst/exam/DisplayMessageActivity.java
+++ app/src/main/java/com/ryb/konst/exam/DisplayMessageActivity.java
@@ -1,11 +1,33 @@
 package com.ryb.konst.exam;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -24,7 +46,27 @@ public class DisplayMessageActivity extends AppCompatActivity {
         // Создаем текстовое поле
         txtLogin = (TextView) findViewById(R.id.txtLogin);
         txtLogin.setText(sLogin);
-        Toast.makeText(this, sLogin, Toast.LENGTH_LONG).show();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        final ArrayList<Date> q = new ArrayList<Date>();
+
+        db.collection("quiz").document("3BG0zYwsre22ES0Zzn32").collection("questions")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getString("text"));  //getData().values());
+                                q.add(document.getDate("createdAt"));
+                            }
+                            System.out.println("array" + q);
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
 
     }
 }
